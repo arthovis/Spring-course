3994425e2ec19d80c38b4e3b4760b54719a7ba64
Arthovis <andreifbaicu@gmail.com>
2019-11-02 11:31:08 +0200

    Initial commit

---
+++ spring-core/src/main/java/com/sda/spring/core/configs/beans/DemoBeanConfig.java
@@ -0,0 +1,16 @@
+package com.sda.spring.core.configs.beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DemoBeanConfig {
+    public static void main(String[] args) {
+            //creates context
+            ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfig.class);
+
+            //get bean
+            MyBean myBean =appContext.getBean("myBean", MyBean.class);
+            myBean.complexOperation();
+        }
+    }
+
---
+++ spring-core/src/main/java/com/sda/spring/core/configs/beans/MyBean.java
@@ -0,0 +1,11 @@
+package com.sda.spring.core.configs.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBean {
+
+    public void complexOperation() {
+        System.out.println("Complex Stuff");
+    }
+}
